b853c39ab57c14dd936f82c9890ef868711346f8
Ivan Pyatanin <ivan.pyatanin@yandex.ru>
2023-09-07 19:03:12 +0300

    Second commit

--- Select.sql
+++ Select.sql
@@ -1,7 +1,6 @@
 --Самый длинный трек
 select name, duration from tracks
-	order by duration desc
-	limit 1;
+	where duration = (select max(duration)from tracks);
 
 --Треки длинее 3,5 минут
 select name, duration from tracks
@@ -12,8 +11,8 @@ select name from Collectin
 	where data >= '2018-01-01' and data <= '2020-12-31';
 
 --Имя состоит из одного слова
-SELECT name FROM Musicians
-	WHERE (LENGTH(name) - LENGTH(REPLACE(name, ' ', ''))+1)=1;
+select name from musicians m
+	where name not like '% %';
 
 --Треки с "мой" или "my"
 select name from tracks
@@ -26,20 +25,22 @@ select jenresid, count(jenresid) as Count from J_M m
 
 --колличество треков в альбомах
 select id, count(t.collection_id)  from T_C t, Collectin c
-	where c."data"  >= '2019-01-01' and c."data" <= '2020-12-31'
-	and id = t.collection_id
+	where c."data"  >= '2019-01-01' and c."data" <= '2020-12-31' 
+	and id = t.collection_id  
 	group by id
-
-
+	
+	
 --средняя продолжительность трека
 select t.collection_id , avg(t2.duration) from T_C t, tracks t2
 	where t.collection_id = t2.id
 	group by t.collection_id
+	
+--исполнители которые не выпускали альбомы в 2020
+select m."name", a."data"  from musicians m, albums a
+	where a."data" not between '2020-01-01' and '2020-12-31' and m.id = a.id ;
 
---исполнители которые не выпускали после в 2020
-select m."name"  from musicians m, albums a
-	where a."data" <= '2019-12-31'
-	and a.id = m.id
---сборники с исполнителем
-select c."name"  from collectin c, musicians m
-	where m."name" like 'Marsh%'
+select c."name"  from musicians m
+	join albums a on m.id = a.id
+	join tracks t on a.id = t.albumid
+	join collectin c on t.id = c.id
+	where m."name" like 'Gene Klien';
